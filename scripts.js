76f2573fbe68dca3d3f9d03f35297e31fea8c414
Mason <Mudland2721@gamil.com>
2020-05-03 15:04:23 -0600

    Made ajax call and cleared search button on submit

--- scripts.js
+++ scripts.js
@@ -1,14 +1,45 @@
-$(document).ready(function () {
-  var place = $("input").val();
-  console.log($("input").val());
+$( document ).ready(function() {
+  console.log( "ready!" );
 
-  function searchWeather() {
-    $.ajax({
-      url:
-        "http://api.openweathermap.org/data/2.5/forecast?q=Utah&appid=14953bc813c991ed3bfed89ef9581de7",
-      method: "GET",
-    }).then(function (response) {});
-    // console.log(response);
+
+  
+
+//search button 
+$('button').on('click', function (event){
+  event.preventDefault()
+  // save to local storage
+  // var saveCity = localStorage.setItem(keyname, value)
+  // append
+  var city = $('input').val();
+  console.log(city) 
+  var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=14953bc813c991ed3bfed89ef9581de7"
+
+//api call 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    //clear search field 
+    function clearSearch () {
+     document.getElementById('searchBar').value = " ";
   }
-  searchWeather();
-});
+    clearSearch()
+  })
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}); 
