84c9acfd16751c97ac28243be11edde15ca90e9d
Mason <Mudland2721@gamil.com>
2020-05-03 16:37:16 -0600

    created appendData function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 
+  function appendData(response) {
+    var getCity = response.city.name
+    var temp = response.list[0].main.temp
+    var humidity = response.list[0].main.humidity
+    var forecast = response.list[0].weather[0].main
+    console.log(getCity)
+    console.log(temp)
+    console.log(humidity)
+    console.log(forecast)
 
+    //hot tip maybe move the appendData fun out of ajax just the call 
+
+    // $("#mainCity").css("font-size", "larger")
+    document.getElementById('mainCity').style.fontSize = 'large';
+    $(".display-4").replaceWith(getCity).text
+    $("#Temp").replaceWith(temp).text
+    $("#humidity").replaceWith(humidity).text
+    $("#forecast").replaceWith(forecast).text
+
+
+
+ }
+ //end append data function
   
 
 //search button 
@@ -11,27 +33,31 @@ $('button').on('click', function (event){
   // var saveCity = localStorage.setItem(keyname, value)
   // append
   var city = $('input').val();
-  console.log(city) 
+  // console.log(city) 
   var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=14953bc813c991ed3bfed89ef9581de7"
 
 //api call 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
-    console.log(response)
+  }).then(function (res) {
+    console.log(res)
     //clear search field 
     function clearSearch () {
      document.getElementById('searchBar').value = " ";
   }
     clearSearch()
+    appendData(res)
+
+
+
+
+
+
+
   })
   
 })
-
-
-
-
 
 
 
